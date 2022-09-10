f451bec0c2eb2cf41dfae3fcca9cbbbb9b1d7b70
Liusy12810 <liusy12810@outlook.com>
2022-09-10 23:18:44 +0800

    build project, writing the json schema file

---
+++ lib/paotuan.py
@@ -0,0 +1,19 @@
+from typing import List, Any, Dict
+import json
+
+
+
+class PaoTuan:
+    def __init__(self, member: List[str], config = "config/config.config.json"):
+        """
+        # Paotuan
+
+        """
+        self.member = member
+
+    def add_member(self, new_member):
+        self.member.append(new_member)
+
+def load_config(config_file: str):
+    config = open(config_file)
+    config = json.loads(config)
---
+++ main.py
@@ -0,0 +1,7 @@
+from lib.paotuan import PaoTuan
+
+def main():
+    paotuan = PaoTuan(["aa", "bb"], default)
+
+if __name__ == "__main__":
+    pass
